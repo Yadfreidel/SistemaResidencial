091d97e1429bd25f94fed54a8538d63e14446498
luismartin023 <mkzcompanyofficial@gmail.com>
2026-07-06 16:04:30 -0400

    feat: implementar Repository generico y los 4 repositorios especificos (Apartamento, Inquilino, Contrato, Pago)

--- SistemaResidencial/Repositories/ApartamentoRepository.cs
+++ SistemaResidencial/Repositories/ApartamentoRepository.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SistemaResidencial.Data;
+using SistemaResidencial.Interfaces;
+using SistemaResidencial.Models;
 
 namespace SistemaResidencial.Repositories
 {
-    internal class ApartamentoRepository
+    public class ApartamentoRepository : Repository<Apartamento>, IApartamentoRepository
     {
+        public ApartamentoRepository(ResidencialDbContext context) : base(context)
+        {
+        }
+
+        public List<Apartamento> ObtenerPorEstado(EstadoApartamento estado)
+        {
+            return _dbSet
+                .Include(a => a.Contratos)
+                .Where(a => a.Estado == estado)
+                .ToList();
+        }
     }
 }
--- SistemaResidencial/Repositories/Repository.cs
+++ SistemaResidencial/Repositories/Repository.cs
@@ -1,10 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SistemaResidencial.Data;
+using SistemaResidencial.Interfaces;
 
 namespace SistemaResidencial.Repositories
 {
-    internal class Repository
+    // Implementación genérica del repositorio: usa context.Set<T>() para
+    // acceder a cualquier entidad sin repetir código CRUD.
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly ResidencialDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(ResidencialDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public List<T> ObtenerTodos()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T? ObtenerPorId(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Agregar(T entidad)
+        {
+            _dbSet.Add(entidad);
+            _context.SaveChanges();
+        }
+
+        public void Actualizar(T entidad)
+        {
+            _dbSet.Update(entidad);
+            _context.SaveChanges();
+        }
+
+        public void Eliminar(int id)
+        {
+            var entidad = ObtenerPorId(id);
+            if (entidad != null)
+            {
+                _dbSet.Remove(entidad);
+                _context.SaveChanges();
+            }
+        }
     }
 }
